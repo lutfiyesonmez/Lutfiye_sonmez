b010cdfa3bdb5b3d50da127e001cdb847668fb97
lutfiyesonmez <lutfiyetopselvi@gmail.com>
2021-07-11 15:55:01 +0000

    6.ADIM Yapildi

--- lutfiyesonmez.py
+++
@@ -1,8 +0,0 @@
-# Fill in this file with the code from parsing JSON exercise
-import json
-dosya=open("lutfiyesonmez.json","r")
-json_dosya=json.load(dosya)
-print("API_KEY : ",json_dosya["ad"])
-print("API_KEY : ",json_dosya["soyad"])
-print("API_KEY  : ",json_dosya["access_token"])
-print("API_KEY : ",json_dosya["il"])
